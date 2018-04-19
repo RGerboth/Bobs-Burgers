8a2476ce867c60335024cceb0d4bb8f2d9df0656
Robert Gerboth <email@example.com>
2018-04-19 08:57:49 -0600

    minor code udpates

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,24 @@
-DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS kv6v85de7bdb71cs;
 
-CREATE DATABASE burgers_db;
+CREATE DATABASE kv6v85de7bdb71cs;
+
+USE kv6v85de7bdb71cs;
+
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(100),
+	eaten BOOL DEFAULT false,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id)
+);
 
 USE burgers_db;
 
-CREATE TABLE products (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR(30),
-	eaten BOOL DEFAULT false,
-	PRIMARY KEY(id)
-);
+INSERT INTO burgers (burger_name, eaten)
+VALUES ("Bacon Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, eaten)
+VALUES ("All American Burger", false);
+INSERT INTO burgers (burger_name, eaten)
+VALUES ("Bonanza Burger", false);
+
+SELECT * FROM burgers;
